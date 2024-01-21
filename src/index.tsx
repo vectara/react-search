641a63a4b50f3a9166e09b1d85d7e3b65c921499
Deryk DeGuzman <deryk@vectara.com>
2024-01-21 13:44:25 -0800

    chore: remove react-icons dependency

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,8 @@ import {
   useEffect,
   useMemo
 } from "react";
-import { BiSearch } from "react-icons/bi";
 import getUuid from "uuid-by-string";
-import { VuiFlexContainer, VuiFlexItem, VuiIcon, VuiSpinner, VuiText } from "./vui";
+import { VuiFlexContainer, VuiFlexItem, VuiSpinner, VuiText } from "./vui";
 import { DeserializedSearchResult } from "./types";
 import { useSearch } from "./useSearch";
 import { SearchResult } from "./SearchResult";
@@ -243,9 +242,7 @@ export const ReactSearch: FC<Props> = ({
               <VuiFlexItem>
                 <VuiFlexContainer alignItems="center" spacing="xs">
                   <VuiFlexItem>
-                    <VuiIcon>
-                      <BiSearch />
-                    </VuiIcon>
+                    <SearchIcon />
                   </VuiFlexItem>
 
                   <VuiFlexItem>
@@ -277,7 +274,7 @@ export const ReactSearch: FC<Props> = ({
                       sendSearchQuery(searchValue);
                     }}
                   >
-                    <BiSearch size="20px" />
+                    <SearchIcon />
                   </button>
                 </div>
               )}
@@ -290,3 +287,28 @@ export const ReactSearch: FC<Props> = ({
     </>
   );
 };
+
+const SearchIcon = () => (
+  <div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      fill="currentColor"
+      height="17px"
+      width="17px"
+      version="1.1"
+      viewBox="-24.52 -24.52 539.44 539.44"
+      xmlSpace="preserve"
+      stroke="currentColor"
+      stroke-width="12"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0" />
+      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+      <g id="SVGRepo_iconCarrier">
+        <g>
+          <path d="M484.1,454.796l-110.5-110.6c29.8-36.3,47.6-82.8,47.6-133.4c0-116.3-94.3-210.6-210.6-210.6S0,94.496,0,210.796 s94.3,210.6,210.6,210.6c50.8,0,97.4-18,133.8-48l110.5,110.5c12.9,11.8,25,4.2,29.2,0C492.5,475.596,492.5,463.096,484.1,454.796z M41.1,210.796c0-93.6,75.9-169.5,169.5-169.5s169.6,75.9,169.6,169.5s-75.9,169.5-169.5,169.5S41.1,304.396,41.1,210.796z" />{" "}
+        </g>
+      </g>
+    </svg>
+  </div>
+);
--- src/vui/components/index.ts
+++ src/vui/components/index.ts
@@ -1,7 +1,5 @@
 import { VuiFlexContainer } from "./flex/FlexContainer";
 import { VuiFlexItem } from "./flex/FlexItem";
-import { VuiIcon } from "./icon/Icon";
-import { ICON_COLOR, ICON_SIZE } from "./icon/types";
 import { VuiPortal } from "./portal/Portal";
 import { VuiScreenBlock } from "./screenBlock/ScreenBlock";
 import { VuiSpinner } from "./spinner/Spinner";
@@ -12,19 +10,16 @@ import { TEXT_COLOR, TEXT_SIZE, TITLE_SIZE } from "./typography/types";
 import { VuiTitle } from "./typography/Title";
 
 export {
-  ICON_COLOR,
-  ICON_SIZE,
   SPINNER_SIZE,
   TEXT_COLOR,
   TEXT_SIZE,
   TITLE_SIZE,
   VuiFlexContainer,
   VuiFlexItem,
-  VuiIcon,
   VuiPortal,
   VuiScreenBlock,
   VuiSpinner,
   VuiText,
   VuiTextColor,
-  VuiTitle,
+  VuiTitle
 };
