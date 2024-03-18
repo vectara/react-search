9524c44e4b01e94e379a36d21452a4a9d33b9afb
mrderyk <deryk@vectara.com>
2024-03-18 10:08:26 -0700

    feat: Add modal close button for small screens (#57)
    * feat: Add modal close button for small screens
    
    * Address comments

--- src/index.tsx
+++ src/index.tsx
@@ -237,7 +237,25 @@ const ReactSearchInternal: FC<Props> = ({
         <SearchModal isOpen={isOpen} onClose={closeModalAndResetResults}>
           <form>
             <div className="vrsSearchForm">
-              <SearchInput value={searchValue} onChange={onChange} onKeyDown={onKeyDown} placeholder={placeholder} />
+              <div className="vrsCloseButtonWrapper">
+                <button
+                  className="vrsSubmitButton"
+                  aria-label="Close"
+                  onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+                    e.preventDefault();
+                    closeModalAndResetResults();
+                  }}
+                >
+                  <CloseIcon size="12px" />
+                </button>
+              </div>
+              <SearchInput
+                value={searchValue}
+                onChange={onChange}
+                onKeyDown={onKeyDown}
+                placeholder={placeholder}
+                autoFocus={true}
+              />
               {isLoading ? (
                 <div className="vrsSubmitButtonWrapper">
                   <VuiSpinner size="xs" />
@@ -288,6 +306,12 @@ const SearchIcon = () => (
       </g>
     </svg>
   </div>
+);
+
+export const CloseIcon = ({ size }: { size: string }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 329.26933 329" width={size} height={size} fill="currentColor">
+    <path d="m194.800781 164.769531 128.210938-128.214843c8.34375-8.339844 8.34375-21.824219 0-30.164063-8.339844-8.339844-21.824219-8.339844-30.164063 0l-128.214844 128.214844-128.210937-128.214844c-8.34375-8.339844-21.824219-8.339844-30.164063 0-8.34375 8.339844-8.34375 21.824219 0 30.164063l128.210938 128.214843-128.210938 128.214844c-8.34375 8.339844-8.34375 21.824219 0 30.164063 4.15625 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921875-2.089844 15.082031-6.25l128.210937-128.214844 128.214844 128.214844c4.160156 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921874-2.089844 15.082031-6.25 8.34375-8.339844 8.34375-21.824219 0-30.164063zm0 0" />
+  </svg>
 );
 
 class ReactSearchWebComponent extends HTMLElement {
